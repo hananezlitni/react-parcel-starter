ffba397de836bbe50304024e05d6787da553e9e6
Hanane Zlitni <hananzlitni@hotmail.com>
2021-03-16 18:45:44 -0400

    Tweak CSS import in index.html

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Main from "./components/Main/Main";
 
 const App = () => {
   return (
     <div className="container">
-      <Header />
       <Main />
       <Footer />
     </div>
